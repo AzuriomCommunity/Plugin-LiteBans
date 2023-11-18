392612611e5c897d2174d51010d82b32beed4513
Esteban Madrid <64392723+Rhbarber@users.noreply.github.com>
2023-11-18 08:46:14 -0500

    Spanish localization (#5)
    * Create admin.php
    
    * Create messages.php

---
+++ resources/lang/es/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'nav' => [
+        'title' => 'LiteBans',
+
+        'settings' => 'Configuración',
+    ],
+
+    'permission' => 'Ver y modificar la configuración de Litebans',
+
+    'settings' => [
+        'title' => 'Configuración del plugin Litebans',
+        'database-connect' => 'Conexión a la base de datos',
+        'other-settings' => 'Otros ajustes',
+
+        'host' => 'Alojamiento',
+        'port' => 'Puerto',
+        'database' => 'Base de Datos',
+        'username' => 'Nombre de Usuario',
+        'password' => 'Contraseña',
+        'perpage' => 'Sanciones por Página',
+        'prefix' => 'Prefijo de la Tabla',
+    ],
+];
---
+++ resources/lang/es/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'navigation' => [
+        'bans' => 'Baneos',
+        'mutes' => 'Silencios',
+        'kicks' => 'Expulsiones',
+        'warns' => 'Advertencias',
+        'search' => 'Buscar',
+    ],
+
+    'title' => 'Sanciones',
+    'plugin_name' => 'Litebans / Sanciones',
+    'history' => 'Historial',
+
+    'username' => 'Pseudo',
+    'staff_ban' => 'Baneado por',
+    'staff_mute' => 'Silenciado por',
+    'staff_kick' => 'Expulsado por',
+    'staff_warn' => 'Advertido por',
+    'staff_punish' => 'Sancionado por',
+    'reason' => 'Razón',
+    'expires_at' => 'Expira en',
+    'no_punishments_found' => 'No encontramos ninguna sanción en la base de datos.',
+
+    'active' => 'Activo',
+    'finished' => 'Finalizado',
+    'unbanned' => 'Sin Sanción',
+    'expired' => 'Caducado',
+    'permanent' => 'Permanente',
+
+    'given_punishments' => 'Castigos impuestos',
+    'staff' => 'Staff',
+];
