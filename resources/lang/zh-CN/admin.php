bd0bbe3c52092a5afaf45dfab45866aa16e7b340
jhqwqmc <113768629+jhqwqmc@users.noreply.github.com>
2024-03-07 15:53:59 +0800

    Add zh-CN (#7)
    * Create admin.php
    
    * Create messages.php
    
    * Rename admin.php to admin.php
    
    * Rename messages.php to messages.php

---
+++ resources/lang/zh-CN/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'nav' => [
+        'title' => 'LiteBans',
+
+        'settings' => '设置',
+    ],
+
+    'permission' => '查看和修改 Litebans 插件设置',
+
+    'settings' => [
+        'title' => 'LiteBans 插件设置',
+        'database-connect' => '数据库连接',
+        'other-settings' => '其他设置',
+
+        'host' => '主机',
+        'port' => '端口',
+        'database' => '数据库',
+        'username' => '用户名',
+        'password' => '密码',
+        'perpage' => '每页惩罚数',
+        'prefix' => '表前缀',
+        'features' => [
+            'title' => '已启用功能',
+            'kicks' => '已启用踢出',
+            'mutes' => '已启用禁言',
+            'warns' => '已启用警告'
+        ]
+    ],
+];
