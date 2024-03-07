4a4fb86e95fcc57aedc0469919f3748b7f671a8e
jhqwqmc <113768629+jhqwqmc@users.noreply.github.com>
2024-03-07 10:38:30 +0800

    Create messages.php

---
+++ resources/lang/zh_cn/messages.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'navigation' => [
+        'bans' => '封禁',
+        'mutes' => '禁言',
+        'kicks' => '踢出',
+        'warns' => '警告',
+        'search' => '搜索',
+    ],
+
+    'title' => '惩罚',
+    'plugin_name' => 'Litebans / 惩罚',
+    'history' => '历史记录',
+
+    'username' => '虚拟',
+    'staff_ban' => '封禁由',
+    'staff_mute' => '禁言由',
+    'staff_kick' => '踢出由',
+    'staff_warn' => '警告由',
+    'staff_punish' => '惩罚由',
+    'reason' => '原因',
+    'expires_at' => '到期时间',
+    'no_punishments_found' => '我们在数据库中没有发现任何惩罚措施。',
+
+    'active' => '进行中',
+    'finished' => '结束',
+    'unbanned' => '解封',
+    'unmuted' => '解除禁言',
+    'expired' => '已到期',
+    'permanent' => '永远',
+
+    'given_punishments' => '给予处罚',
+    'staff' => '工作人员',
+];
